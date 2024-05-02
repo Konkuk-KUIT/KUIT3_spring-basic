3f45955fcf3f611ab130b393e92c7afaafbdbd49
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-05-02 16:08:43 +0900

    Feat: 1단계

--- src/main/java/kuit/springbasic/controller/LoginController.java
+++ src/main/java/kuit/springbasic/controller/LoginController.java
@@ -1,15 +1,39 @@
 package kuit.springbasic.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import kuit.springbasic.db.MemoryUserRepository;
+import kuit.springbasic.domain.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j //로그 기록을 위한 lombok 라이브러리 어노테이션
+@Controller
+@RequiredArgsConstructor // lombok 라이브러리의 어노테이션으 필수 의존성을 주입받는 생성자 자동 생성
 public class LoginController {
 
+    private final MemoryUserRepository memoryUserRepository;
 
     /**
      * TODO: showLoginForm
      */
+    @RequestMapping("user/loginForm")
+    public String showLoginForm() {
+        log.info("LoginController.showLoginForm");
+        return "/user/login";
+    }
 
     /**
      * TODO: showLoginFailed
      */
+    @RequestMapping("user/loginFailed")
+    public String showLoginFailed() {
+        log.info("LoginController.showLoginFailed");
+        return "/user/loginFailed";
+    }
 
     /**
      * TODO: login
@@ -18,9 +42,33 @@ public class LoginController {
      * loginV3 : @RequestParam 생략(비추천)
      * loginV4 : @ModelAttribute
      */
+    @RequestMapping("/user/login")
+    public String login(@RequestParam String userId, @RequestParam String password, HttpServletRequest request) {
+        log.info("LoginController.login");
+
+        User comparedUser = new User(userId,password);
+        User user = memoryUserRepository.findByUserId(userId);
+        System.out.println(comparedUser);
+        if (user != null && user.equals(comparedUser)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            return "redirect:/";
+        }
+        return "redirect:/user/loginFailed";
+    }
 
     /**
      * TODO: logout
      */
+    @RequestMapping("/user/logout")
+    public String logout(HttpServletRequest request) {
+        log.info("LoginController.logout");
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/";
+    }
 
 }
--- src/main/java/kuit/springbasic/controller/UserController.java
+++ src/main/java/kuit/springbasic/controller/UserController.java
@@ -1,11 +1,34 @@
 package kuit.springbasic.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import kuit.springbasic.db.MemoryUserRepository;
+import kuit.springbasic.domain.User;
+import kuit.springbasic.util.UserSessionUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j //로그 기록을 위한 lombok 라이브러리 어노테이션
+@Controller
+@RequiredArgsConstructor // lombok 라이브러리의 어노테이션의 필수 의존성을 주입받는 생성자 자동 생성
+// 초기화되지 않은 final 필드에 대해 생성자를 생성해줌 DI 편의성
+
 public class UserController {
 
+    private final MemoryUserRepository memoryUserRepository;
 
     /**
      * TODO: showUserForm
      */
+    @RequestMapping("/user/form")
+    public String showUserForm() {
+        log.info("UserController.showUserForm");
+        return "/user/form";
+    }
 
     /**
      * TODO: createUser
@@ -13,18 +36,58 @@ public class UserController {
      * createUserV2 : @ModelAttribute
      */
 
+    @RequestMapping("/user/signup")
+    public String createUserV1(@RequestParam String userId, @RequestParam String password, @RequestParam String name, @RequestParam String email) {
+        log.info("UserController.createUserV1");
+        User user = new User(userId, password, name, email);
+        memoryUserRepository.insert(user);
+
+        return "redirect:/user/loginForm";
+    }
+    // public String createUserV2
+
     /**
      * TODO: showUserList
      */
+    @RequestMapping("/user/list")
+    public String showUserList(HttpServletRequest request, Model model) {
+        log.info("UserController.showUserList");
+
+        HttpSession session = request.getSession();
+        if (UserSessionUtils.isLoggedIn(session)) {
+            model.addAttribute("users", memoryUserRepository.findAll());
+            return "/user/list";
+        }
+        return "redirect:/user/loginForm";
+    }
 
     /**
      * TODO: showUserUpdateForm
      */
+    @RequestMapping("/user/updateForm")
+    public String showUserUpdateForm(@RequestParam String userId, Model model) {
+        log.info("UserController.showUserUpdateForm");
+
+        User user = memoryUserRepository.findByUserId(userId);
+        if (user != null) {
+            model.addAttribute("user", user);
+            return "/user/updateForm";
+        }
+        return "/home";
+    }
 
     /**
      * TODO: updateUser
      * updateUserV1 : @RequestParam
      * updateUserV2 : @ModelAttribute
      */
+    @RequestMapping("/user/update")
+    public String updateUserV1(@RequestParam String userId, @RequestParam String password, @RequestParam String name, @RequestParam String email) {
+        log.info("UserController.updateUserV1");
+        User user = new User(userId, password, name, email);
+        memoryUserRepository.update(user);
 
+        return "redirect:/user/list";
+    }
+    // public String updateUserV2
 }
