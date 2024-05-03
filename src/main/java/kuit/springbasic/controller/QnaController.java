03dd3e68204c8d412aaaf3246c654f11799821b9
김민석 <skyfox001015@naver.com>
2024-05-03 17:45:53 +0900

    feat : ShowQna추가, navigaion 수정

--- src/main/java/kuit/springbasic/controller/QnaController.java
+++ src/main/java/kuit/springbasic/controller/QnaController.java
@@ -1,8 +1,14 @@
 package kuit.springbasic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class QnaController {
+
+    @RequestMapping("/qna/show")
+    public String showQna(){
+        return "/qna/show";
+    }
 
 }
