625a4246fd1d036ec2b3edd6c6fff1e4a2e14481
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2024-05-03 18:24:13 +0900

    [feat] requirement devleop complete

--- src/main/java/kuit/springbasic/controller/LoginController.java
+++ src/main/java/kuit/springbasic/controller/LoginController.java
@@ -1,15 +1,41 @@
 package kuit.springbasic.controller;
 
-public class LoginController {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import kuit.springbasic.db.MemoryUserRepository;
+import kuit.springbasic.domain.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.net.http.HttpRequest;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class LoginController {
+    private final MemoryUserRepository userRepository;
 
     /**
      * TODO: showLoginForm
      */
+    @RequestMapping("/loginForm")
+    public String showLoginForm(){
+
+        return "/user/login";
+    }
 
     /**
      * TODO: showLoginFailed
      */
+    @RequestMapping("/loginFailed")
+    public String showLoginFailed(){
+        return "/user/loginFailed";
+    }
 
     /**
      * TODO: login
@@ -18,9 +44,24 @@ public class LoginController {
      * loginV3 : @RequestParam 생략(비추천)
      * loginV4 : @ModelAttribute
      */
-
+    @RequestMapping("/login")
+    public ModelAndView doLogin(@RequestParam("userId") String userId, @RequestParam("password") String password,
+                                HttpServletRequest req){
+        log.info("LoginController.dologin");
+        User user=userRepository.findByUserId(userId);
+        if(user != null && user.matchPassword(password)){
+            req.getSession().setAttribute("user",user);
+            return new ModelAndView("redirect:/");
+        }
+        return new ModelAndView("redirect:/user/loginFailed");
+    }
     /**
      * TODO: logout
      */
-
+    @RequestMapping("/logout")
+    public ModelAndView doLogout(HttpServletRequest req){
+        HttpSession session = req.getSession();
+        session.removeAttribute("user");
+        return new ModelAndView( "redirect:/");
+    }
 }
--- src/main/java/kuit/springbasic/controller/qna/QnAController.java
+++ src/main/java/kuit/springbasic/controller/qna/QnAController.java
@@ -1,10 +1,49 @@
 package kuit.springbasic.controller.qna;
 
 
+import kuit.springbasic.db.MemoryAnswerRepository;
+import kuit.springbasic.db.MemoryQuestionRepository;
+import kuit.springbasic.domain.Answer;
+import kuit.springbasic.domain.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+@Controller
+@RequestMapping("/qna")
 public class QnAController {
+
+    final private MemoryQuestionRepository questionRepository;
+    final private MemoryAnswerRepository memoryAnswerRepository;
+    @Autowired
+    public QnAController(MemoryQuestionRepository QRepository,MemoryAnswerRepository ARepository) {
+        this.questionRepository = QRepository;
+        this.memoryAnswerRepository= ARepository;
+    }
+
     /**
      * TODO: showQnA
      */
+    @RequestMapping("/show")
+    public ModelAndView showQnA(@RequestParam int questionId){
+        //Long questionId = Long.parseLong(req.getParameter("questionId"));
+        //System.out.println("qusetion ID"+questionId);
+
+        Question question = questionRepository.findByQuestionId(questionId);
+        List<Answer> answers = memoryAnswerRepository.findAllByQuestionId(questionId);
+
+        ModelAndView QnA = new ModelAndView("/qna/show");
+        return QnA
+                .addObject("question", question)
+                .addObject("answers", answers);
+
+
+    }
+
+
 
 
 }
