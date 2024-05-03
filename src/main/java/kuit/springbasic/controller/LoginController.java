02af3ca5aa1bb8395339b991ef7ea300c97d9f39
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-05-03 16:18:00 +0900

    feat: LoginController loginForm, loginFailed, loginV1,V2

--- src/main/java/kuit/springbasic/controller/LoginController.java
+++ src/main/java/kuit/springbasic/controller/LoginController.java
@@ -1,23 +1,55 @@
 package kuit.springbasic.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import kuit.springbasic.db.MemoryUserRepository;
+import kuit.springbasic.domain.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class LoginController {
 
+    private final MemoryUserRepository memoryUserRepository;
 
-    /**
-     * TODO: showLoginForm
-     */
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "/user/login";
+    }
 
-    /**
-     * TODO: showLoginFailed
-     */
+    @GetMapping("/loginFailed")
+    public String loginFailed() {
+        return "/user/loginFailed";
+    }
 
-    /**
-     * TODO: login
-     * loginV1 : @RequestParam("")
-     * loginV2 : @RequestParam
-     * loginV3 : @RequestParam 생략(비추천)
-     * loginV4 : @ModelAttribute
-     */
+    @PostMapping("/loginV1")
+    public ModelAndView doLoginV1(@RequestParam("userId") String userId, @RequestParam("password") String password,
+                          HttpServletRequest request) {
+        log.info("LoginController.doLoginV1");
+        User user = memoryUserRepository.findByUserId(userId);
+        if (user != null && user.isSameUser(userId, password)) {
+            request.getSession().setAttribute("user", user);
+            return new ModelAndView("redirect:/");
+        }
+        return new ModelAndView("redirect:/user/loginFailed");
+    }
+
+    @PostMapping("/login")
+    // @ModelAttribute : 기본생성자와 Setter 이용해 알아서 인자를 객체로 만들어줌
+    public ModelAndView doLoginV2(@ModelAttribute User loggedinUser, HttpServletRequest request) {
+        log.info("LoginController.doLoginV2");
+        User user = memoryUserRepository.findByUserId(loggedinUser.getUserId());
+        if (user != null && user.isSameUser(loggedinUser.getUserId(), loggedinUser.getPassword())) {
+            request.getSession().setAttribute("user", user);
+            return new ModelAndView("redirect:/");
+        }
+        return new ModelAndView("redirect:/user/loginFailed");
+    }
 
     /**
      * TODO: logout
