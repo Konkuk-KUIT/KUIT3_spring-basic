636964b5a008eb085bd7f53422aa3c37e4390307
김민석 <skyfox001015@naver.com>
2024-05-03 16:34:11 +0900

    feat : showUserForm완료

--- src/main/java/kuit/springbasic/controller/UserController.java
+++ src/main/java/kuit/springbasic/controller/UserController.java
@@ -1,17 +1,48 @@
 package kuit.springbasic.controller;
 
+import kuit.springbasic.db.MemoryUserRepository;
+import kuit.springbasic.domain.User;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
 public class UserController {
 
+    private final MemoryUserRepository memoryUserRepository;
+    
+    @RequestMapping("/user/form")
+    public String showUserForm(@ModelAttribute User signningUser){
+        return "/user/form";
 
-    /**
-     * TODO: showUserForm
-     */
+    }
+
 
     /**
      * TODO: createUser
      * createUserV1 : @RequestParam
      * createUserV2 : @ModelAttribute
      */
+//    @RequestMapping("/user/form")
+//
+//    public ModelAndView showUserForm(@ModelAttribute User signningUser){
+//        User user = new User(signningUser.getUserId(),
+//                signningUser.getPassword(),
+//                signningUser.getName(),
+//                signningUser.getEmail());
+//        memoryUserRepository.insert(user);
+//        return new ModelAndView( "redirect:/user/userList");
+//
+//    }
+
 
     /**
      * TODO: showUserList
