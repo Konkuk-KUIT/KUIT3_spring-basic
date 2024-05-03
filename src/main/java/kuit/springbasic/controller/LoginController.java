19fd17637bae12319501d6c37b97980dd39ceb39
김민석 <skyfox001015@naver.com>
2024-05-03 16:08:57 +0900

    feat : User Class @Getter,@Setter, @Noarg,@Allarg, loginFailed,login 완료

--- src/main/java/kuit/springbasic/controller/LoginController.java
+++ src/main/java/kuit/springbasic/controller/LoginController.java
@@ -1,15 +1,42 @@
 package kuit.springbasic.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import kuit.springbasic.db.MemoryUserRepository;
+import kuit.springbasic.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class LoginController {
 
+    private final MemoryUserRepository memoryUserRepository;
 
     /**
      * TODO: showLoginForm
      */
+    @RequestMapping("/loginForm")
+    public String showLoginForm(){
+        return "/user/login";
+    }
+
 
     /**
      * TODO: showLoginFailed
      */
+    @RequestMapping("/loginFailed")
+    public String showLoginFailed(){
+        return "user/loginFailed";
+    }
+
+
 
     /**
      * TODO: login
@@ -18,6 +45,30 @@ public class LoginController {
      * loginV3 : @RequestParam 생략(비추천)
      * loginV4 : @ModelAttribute
      */
+
+//    @RequestMapping("/login")
+//    public ModelAndView login(@RequestParam("userId") String userId, @RequestParam("password") String password, HttpServletRequest request){
+//        User user = memoryUserRepository.findByUserId(userId);
+//
+//        if (user != null && user.isSameUser(userId, password)) {
+//            request.getSession().setAttribute("user", user);
+//            return new ModelAndView("redirect:/");
+//        }
+//        return new ModelAndView("redirect:/user/loginFailed");
+//
+//    }
+
+    @RequestMapping("/login")
+    public ModelAndView login(@ModelAttribute User loggedUser, HttpServletRequest request){
+        User user = memoryUserRepository.findByUserId(loggedUser.getUserId());
+
+        if (user != null && user.isSameUser(loggedUser.getUserId(), loggedUser.getPassword())) {
+            request.getSession().setAttribute("user", user);
+            return new ModelAndView("redirect:/");
+        }
+        return new ModelAndView("redirect:/user/loginFailed");
+    }
+
 
     /**
      * TODO: logout
