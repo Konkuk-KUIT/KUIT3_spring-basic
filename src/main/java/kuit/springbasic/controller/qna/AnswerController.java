4d8e209fcefd7d560441dbac89b1393594d3557d
Minseok <saltishDev@gmail.com>
2024-05-04 23:25:22 +0900

    Feat: AnswerController 구현

--- src/main/java/kuit/springbasic/controller/qna/AnswerController.java
+++ src/main/java/kuit/springbasic/controller/qna/AnswerController.java
@@ -1,7 +1,26 @@
 package kuit.springbasic.controller.qna;
 
+import kuit.springbasic.db.MemoryAnswerRepository;
+import kuit.springbasic.db.MemoryQuestionRepository;
+import kuit.springbasic.domain.Answer;
+import kuit.springbasic.domain.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
 public class AnswerController {
 
+    private final MemoryQuestionRepository memoryQuestionRepository;
+    private final MemoryAnswerRepository memoryAnswerRepository;
 
     /**
      * TODO: addAnswer - @PostMapping
@@ -11,4 +30,14 @@ public class AnswerController {
      * addAnswerV3 : @ModelAttribute, @ResponseBody
      */
 
+    @PostMapping("/api/qna/addAnswer")
+    public Answer addAnswer(@ModelAttribute Answer answer){
+        Answer savedAnswer = memoryAnswerRepository.insert(answer);
+
+        Question question = memoryQuestionRepository.findByQuestionId(answer.getQuestionId());
+        question.increaseCountOfAnswer();
+        memoryQuestionRepository.update(question);
+
+        return savedAnswer;
+    }
 }
