e835cb6a70d4357dbc005069fa043251530b5992
djlim00 <127932430+djlim00@users.noreply.github.com>
2024-05-04 23:09:04 +0900

    2
    2

--- src/main/java/kuit/springbasic/controller/LoginController.java
+++ src/main/java/kuit/springbasic/controller/LoginController.java
@@ -43,10 +43,35 @@ public class LoginController {
 
     }
 
+    //@RequestMapping("/login")
+    public ModelAndView doLoginV2(@RequestParam String userId, @RequestParam String password, HttpServletRequest request){
+        log.info("LoginController.dologinV2");
+        User user = memoryUserRepository.findByUserId(userId);
+        if(user !=null && user.isSameUser(userId,password)){
+            request.getSession().setAttribute("user",user);
+            return new ModelAndView("redirect:/");
+        }
+        return new ModelAndView("redirect:/user/loginFailed");
+
+    }
+
+    //@RequestMapping("/login")
+    public ModelAndView doLoginV3(String userId, String password, HttpServletRequest request){
+        log.info("LoginController.dologinV3");
+        User user = memoryUserRepository.findByUserId(userId);
+        if(user !=null && user.isSameUser(userId,password)){
+            request.getSession().setAttribute("user",user);
+            return new ModelAndView("redirect:/");
+        }
+        return new ModelAndView("redirect:/user/loginFailed");
+
+    }
+
+
     // 이 방법은 getter, setter가 있어야함
     @RequestMapping("/login")
-    public ModelAndView doLoginV2(@ModelAttribute User loggedinUser, HttpServletRequest request){
-        log.info("LoginController.dologinV2");
+    public ModelAndView doLoginV4(@ModelAttribute User loggedinUser, HttpServletRequest request){
+        log.info("LoginController.dologinV4");
         User user = memoryUserRepository.findByUserId(loggedinUser.getUserId());
         if(user != null && user.isSameUser(loggedinUser.getUserId(), loggedinUser.getPassword())){
             request.getSession().setAttribute("user",user);
@@ -55,22 +80,14 @@ public class LoginController {
         return new ModelAndView("redirect:/user/loginFailed");
     }
 
+    @RequestMapping("/loginFaild")
+    public ModelAndView logout(@ModelAttribute User loggedoutUser, HttpServletRequest request){
+        log.info("LogOutController");
+        User user = memoryUserRepository.findByUserId(loggedoutUser.getUserId());
+        request.getSession().invalidate();
+        return new ModelAndView("redirect:/");
+    }
 
-    /**
-     * TODO: showLoginForm
-     */
-
-    /**
-     * TODO: showLoginFailed
-     */
-
-    /**
-     * TODO: login
-     * loginV1 : @RequestParam("")
-     * loginV2 : @RequestParam
-     * loginV3 : @RequestParam 생략(비추천)
-     * loginV4 : @ModelAttribute
-     */
 
     /**
      * TODO: logout
--- src/main/java/kuit/springbasic/controller/UserController.java
+++ src/main/java/kuit/springbasic/controller/UserController.java
@@ -1,24 +1,56 @@
 package kuit.springbasic.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kuit.springbasic.db.MemoryUserRepository;
+import kuit.springbasic.domain.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
+    private final MemoryUserRepository memoryUserRepository;
+    @RequestMapping("/form")
+    public String showForm(){
+        return "/user/form";
+    }
 
-    /**
-     * TODO: showUserForm
-     */
 
     /**
      * TODO: createUser
      * createUserV1 : @RequestParam
      * createUserV2 : @ModelAttribute
      */
+    @RequestMapping("/signup")
+    public ModelAndView createUser(@RequestParam("userId") String userId,
+                                   @RequestParam("password") String password,
+                                   @RequestParam("name") String name,
+                                   @RequestParam("email") String email) {
+        log.info("CreateUserController.createUser");
+
+        User user = new User(userId, password, name, email);
+        memoryUserRepository.insert(user);
+
+        // Redirect to userList page
+        return new ModelAndView("redirect:/v1/user/userList");
+    }
+
 
     /**
      * TODO: showUserList
      */
+    @RequestMapping("/userList")
+    public String showList(){return "/user/list";}
 
     /**
      * TODO: showUserUpdateForm
