6821b4c75277ae22af4282aeebd291b05a3118a7
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-05-04 17:04:53 +0900

    feat: 미션 완료

--- src/main/java/kuit/springbasic/controller/LoginController.java
+++ src/main/java/kuit/springbasic/controller/LoginController.java
@@ -1,26 +1,58 @@
 package kuit.springbasic.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import kuit.springbasic.db.MemoryUserRepository;
+import kuit.springbasic.domain.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class LoginController {
 
+    private final MemoryUserRepository memoryUserRepository;
 
-    /**
-     * TODO: showLoginForm
-     */
+    @GetMapping("/loginForm")
+    public String showLoginForm(){
+        return "/user/login";
+    }
 
-    /**
-     * TODO: showLoginFailed
-     */
+    @GetMapping("/loginFailed")
+    public String showLoginFailed(){
+        return "/user/loginFailed";
+    }
 
-    /**
-     * TODO: login
-     * loginV1 : @RequestParam("")
-     * loginV2 : @RequestParam
-     * loginV3 : @RequestParam 생략(비추천)
-     * loginV4 : @ModelAttribute
-     */
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute User loggedinUser, HttpServletRequest request){
+        User user = memoryUserRepository.findByUserId(loggedinUser.getUserId());
+
+        if(user!= null && user.isSameUser(loggedinUser.getUserId(), loggedinUser.getPassword())){
+            request.getSession().setAttribute("user", user);
+            return new ModelAndView("redirect:/");
+        }
+
+
+        return new ModelAndView("redirect:/user/loginFailed");
+    }
+
+
 
     /**
      * TODO: logout
      */
+
+    @GetMapping("/logout")
+    public String logout(@ModelAttribute User user, HttpServletRequest request){
+        request.getSession().removeAttribute("user");
+        return "redirect:/";
+    }
 
 }
--- src/main/java/kuit/springbasic/controller/UserController.java
+++ src/main/java/kuit/springbasic/controller/UserController.java
@@ -1,30 +1,54 @@
 package kuit.springbasic.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import kuit.springbasic.db.MemoryUserRepository;
+import kuit.springbasic.domain.User;
+import kuit.springbasic.util.UserSessionUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final MemoryUserRepository memoryUserRepository;
 
-    /**
-     * TODO: showUserForm
-     */
+    @GetMapping("/form")
+    public String showUserForm(){
+        return "/user/form";
+    }
 
-    /**
-     * TODO: createUser
-     * createUserV1 : @RequestParam
-     * createUserV2 : @ModelAttribute
-     */
+    @PostMapping("/signup")
+    public String createUser(@ModelAttribute User signUpUser){
+        User user = new User(signUpUser.getUserId(), signUpUser.getPassword(), signUpUser.getName(), signUpUser.getEmail());
+        memoryUserRepository.insert(user);
+        return "redirect:/user/list";
+    }
 
-    /**
-     * TODO: showUserList
-     */
+    @GetMapping("/list")
+    public ModelAndView showUserList(HttpServletRequest request){
+        if(UserSessionUtils.isLoggedIn(request.getSession())){
+            return new ModelAndView("user/list").addObject("users", memoryUserRepository.findAll());
+        }
+        return new ModelAndView("redirect:/user/loginForm");
+    }
 
-    /**
-     * TODO: showUserUpdateForm
-     */
 
-    /**
-     * TODO: updateUser
-     * updateUserV1 : @RequestParam
-     * updateUserV2 : @ModelAttribute
-     */
+    @GetMapping("/updateForm")
+    public String showUserUpdateForm(){
+        return "/user/updateForm";
+    }
+
+    @PostMapping("/update")
+    public String updateUserV2(@ModelAttribute User updateUser){
+        memoryUserRepository.changeUserInfo(updateUser);
+        return "redirect:/user/list";
+    }
 
 }
